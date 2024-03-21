ea8cc50ee6529893d69400c583a38e1f6d10588c
Ðyamabiki <theoriginalsok123@gmail.com>
2024-03-21 22:57:00 +0200

    changed lab from level 2 to 3

--- lab2var2lvl2.py
+++
@@ -1,9 +0,0 @@
-def min_paint(k, t, lengths):
-    max_length = max(lengths)
-    total_length = sum(lengths)
-
-    if k >= len(lengths):
-        return max_length * t
-
-    paint_per_worker = total_length / k
-    return max(max_length, paint_per_worker) * t
