f54c272914f7b902f53f0e5e951a6f9a5c4b0668
Ðyamabiki <theoriginalsok123@gmail.com>
2024-05-03 00:54:07 +0300

    Added lab8 with unittest and config files

--- test/primwells_test.py
+++ test/primwells_test.py
@@ -3,13 +3,12 @@ import unittest
 from src.primwells import min_cable_length
 
 
-class TestCommunicationWells(unittest.TestCase):
-    def test_based_testcase(self):
+class TestWells(unittest.TestCase):
+    def test_hasconnections_testcase(self):
         file = "communication_wells.csv"
         out = "../test/result_communication_wells.txt"
 
         min_cable_length(file, out)
-        # base test case
         with open(out) as output_file:
             output = output_file.read()
 
@@ -18,7 +17,7 @@ class TestCommunicationWells(unittest.TestCase):
 
 
 class TestCommunicationDisconnect(unittest.TestCase):
-    def test_based_testcase(self):
+    def test_noconnections_testcase(self):
         file = "../test/communication_wells_with_disconnection.csv"
         out = "../test/result_communication_wells_with_disconnection.txt"
 
@@ -26,8 +25,7 @@ class TestCommunicationDisconnect(unittest.TestCase):
 
         with open(out) as output_file:
             output = output_file.read()
-        # test case if no graph
-        expected = "-1"
+            expected = '-1'
         self.assertEqual(output, expected)
 
 
