3ac643480df75b0c41036e87a85430220bcc6a4e
yamabiki <theoriginalsok123@gmail.com>
2024-05-20 10:49:02 +0300

    changed lab9

--- src/dynamic_electrics.py
+++ src/dynamic_electrics.py
@@ -1,27 +1,30 @@
 import math
 
+def calculate_distance(width, height):
+    return math.sqrt(width ** 2 + height ** 2)
 
-def max_length(width, heights):
-    num_heights = len(heights)
-    dp = [[0] * (h + 1) for h in heights]
+def max_length(distance, heights):
+    num_columns = len(heights)
+    if num_columns == 1:
+        return 0
+
+    max_top_length = 0
+    max_bottom_length = 0
+
+    for i in range(num_columns - 1):
+        bottom_to_bottom = distance
+        height_difference = abs(heights[i] - heights[i + 1])
+        max_to_max = calculate_distance(distance, height_difference)
+        min_to_max = calculate_distance(distance, max(heights[i + 1] - 1, 0))
+        max_to_min = calculate_distance(distance, max(heights[i] - 1, 0))
+
+        next_top_length = max(max_to_max + max_top_length, min_to_max + max_bottom_length)
+        next_bottom_length = max(max_to_min + max_top_length, bottom_to_bottom + max_bottom_length)
+
+        max_top_length = next_top_length
+        max_bottom_length = next_bottom_length
 
 
-    for height in range(1, heights[0] + 1):
-        dp[0][height] = 0
-    for i in range(1, num_heights):
-        for height2 in range(1, heights[i] + 1):
-            dp[i][height2] = max(
-                dp[i - 1][height1] + calculate_distance(width, height1, height2)
-                for height1 in range(1, heights[i - 1] + 1)
-            )
-
-    max_length = max(dp[-1])
 
 
-    return round(max_length, 2)
-
-
-def calculate_distance(width, height1, height2):
-    return math.sqrt(width ** 2 + (height2 - height1) ** 2)
-
-
+    return round(max(max_top_length, max_bottom_length), 2)
